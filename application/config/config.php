33c276b4def815dff2ef6103c71221737da8fb0c
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2018-12-25 03:48:59 +0700

    hm

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/supo_web/';
+$config['base_url'] = 'http://localhost:8012/supo_web/';
 
 /*
 |--------------------------------------------------------------------------
